828a2b8b097a56a6d9ff4fa386f198a4ef717ecc
xu_mj <331538695@qq.com>
2024-06-15 14:34:32 +0800

    feat: delete target

--- list/target/debug/examples/list.d
+++
@@ -1,5 +0,0 @@
-D:\Projects\rust\rust-prectice\list\target\debug\examples\list.exe: examples\list.rs
-
-D:\Projects\rust\rust-prectice\list\target\debug\examples\list.d: examples\list.rs
-
-examples\list.rs:
--- list/target/debug/list.d
+++
@@ -1,1 +0,0 @@
-D:\Projects\rust\rust-prectice\list\target\debug\list.exe: D:\Projects\rust\rust-prectice\list\src\main.rs
